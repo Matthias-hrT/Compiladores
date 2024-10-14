e2c29c8bd4ef964f53743a3aa6a6624084c6aff2
Hirata <mhvanzela@gmail.com>
2024-10-14 03:02:33 -0300

    N tenho ideia do que fiz, mas eu fiz

--- Compiladores/src/LeitorArq.java
+++ Compiladores/src/LeitorArq.java
@@ -1,54 +1,74 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-
 public class LeitorArq {
-
-    public BufferedReader leitor;
-    private String buffer;
+    private BufferedReader leitor;
+    private char[] buffer1;
+    private char[] buffer2;
     private int index;
+    private int bufferAtual;
+    private boolean usandoBuffer1;
 
-    public LeitorArq(String arq){
+    private static final int TAMANHO_BUFFER = 1024;
+
+    public LeitorArq(String arq) {
         try {
             leitor = new BufferedReader(new FileReader(arq));
-            buffer = "";
+            buffer1 = new char[TAMANHO_BUFFER];
+            buffer2 = new char[TAMANHO_BUFFER];
             index = 0;
+            bufferAtual = 0;
+            usandoBuffer1 = true;
+            carregarProximoBuffer();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public int lerProxCaractere(){
-        if (index < buffer.length()){
-            return buffer.charAt(index++);
-        }
+    private void carregarProximoBuffer() {
         try {
-            int c = leitor.read();
-            if (c != -1){
-                buffer += (char) c;
-                return lerProxCaractere();
+            if (usandoBuffer1) {
+                bufferAtual = leitor.read(buffer1, 0, TAMANHO_BUFFER);
+            } else {
+                bufferAtual = leitor.read(buffer2, 0, TAMANHO_BUFFER);
             }
+            index = 0;  // Reinicializa o índice para o novo buffer
         } catch (IOException e) {
             e.printStackTrace();
-            return -1;
-        }
-        return -1;
-    }
-
-    public void caractereAnterior(){
-        if (index > 0){
-            index --;
         }
     }
 
-    public void fecharArquivo(){
+    public int lerProxCaractere() {
+        // troca o buffer apos terminar de ler o atual
+        if (index >= bufferAtual) {
+            if (bufferAtual == -1) {
+                return -1;
+            }
+            usandoBuffer1 = !usandoBuffer1;  // Alterna os buffers
+            carregarProximoBuffer();
+        }
+
+        // Retorna o prox caractere do buffer
+        if (usandoBuffer1) {
+            return buffer1[index++];
+        } else {
+            return buffer2[index++];
+        }
+    }
+
+    public void caractereAnterior() {
+        // Retrocede o índice
+        if (index > 0) {
+            index--;
+        }
+    }
+
+    public void fecharArquivo() {
         try {
             leitor.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
