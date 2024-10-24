6cdcfbf2e91bf9e5d7fed2952a850376a996d0aa
Hirata <mhvanzela@gmail.com>
2024-10-24 11:30:53 -0300

    a

--- Compiladores/src/Main.java
+++ Compiladores/src/Main.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        AnalLexico lex =  new AnalLexico("C:\\Users\\Hirata\\IdeaProjects\\Compiladores\\programa10.gyh");
+        AnalLexico lex = new AnalLexico("C:\\Users\\Hirata\\IdeaProjects\\Compiladores\\programa2.gyh");
+        List<Token> tokens = new ArrayList<>();
         Token t = lex.proxToken();
-        while(t != null){
-            System.out.println(t.toString());
+
+        while(t != null) {
+            tokens.add(t);  // Adiciona o token à lista
             t = lex.proxToken();
         }
 
+        // Exibe todos os tokens da lista
+        for (Token token : tokens) {
+            System.out.println(token.toString());
+        }
     }
 }
